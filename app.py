f54bf5189277e04bf0fa8c185a971a8406032f23
Samuel Rowe <samuelrowe1999@gmail.com>
2020-10-12 21:54:10 +0530

    Integrated the Flipkart scrapper.

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
-from flask import Flask, render_template,  session, redirect, request
+from flask import Flask, render_template,  session, redirect, request, jsonify
 from flask_cors import CORS,cross_origin
+from scraper import scrap_flipkart
 
 app = Flask(__name__)
 
-@app.route('/',methods=['GET'])  
+@app.route('/', methods=['GET'])
 @cross_origin()
 def homePage():
 	return render_template("index.html")
+
+@app.route('/scrap', methods=['GET'])
+@cross_origin()
+def scrap():
+	search_text = request.args.get("query")
+	result = scrap_flipkart(search_text)
+	return jsonify(result)
 
 if __name__ == '__main__':
 	app.run()
---
+++ scraper.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 23 21:02:51 2020
+
+@author: Dell
+"""
+
+import time
+from bs4 import BeautifulSoup
+from selenium import webdriver
+import json
+
+DRIVER_URL='/usr/lib/chromium-browser/chromedriver'
+
+def scrap_flipkart(search_text):
+    driver = webdriver.Chrome(DRIVER_URL)
+    driver.get('https://www.flipkart.com')
+    time.sleep(5)
+
+    driver.find_element_by_class_name('_2AkmmA._29YdH8').click()
+    time.sleep(5)   
+    user_input = driver.find_element_by_class_name('LM6RPg')
+    user_input.click()
+
+    # initiate a new input variable here to take the apps response instead of Redmi note 5 
+    user_input.send_keys(search_text) # follow as per the app response (refer above line)
+    time.sleep(5)    
+    driver.find_element_by_class_name('vh79eN').click()
+    time.sleep(5)
+    driver.find_element_by_class_name('_3wU53n').click()
+    time.sleep(5)
+    i=driver.switch_to.window(driver.window_handles[1])
+    new=driver.current_url    
+    driver.get(new)
+
+    def strip_read_more(text):
+      if text.endswith("READ MORE"):
+        return text[0:len(text) - 9]
+      return text
+
+    result = []
+    driver.find_element_by_class_name("swINJg._3nrCtb").click()
+    time.sleep(5)
+    page_url=driver.current_url
+    for k in range(1, 3):
+        new_page_url = (str(page_url) + "&page=" + str(k))    
+        driver.get(new_page_url)        
+        [item.click() for item in driver.find_elements_by_class_name("_1EPkIx")]
+        time.sleep(1)
+        driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
+        time.sleep(1)
+        soup = BeautifulSoup(driver.page_source, 'lxml')
+        for items in soup:
+            review_titles = soup.find_all("p",{"class" : "_2xg6Ul"})# rev title
+            review_contents = soup.find_all("div",{"class" : "qwjRop"})#rev
+            customer_names = soup.find_all("p",{"class" : "_3LYOAd _3sxSiS"})#customer name
+            ratings_list = soup.find_all("div",{"class" : "hGSR34 E_uFuv"})#ratings
+            dates = soup.find_all("p",{"class" : "_3LYOAd"})
+            for review_title, review_content, customer_name, ratings, date in zip(review_titles, review_contents, customer_names, ratings_list, dates):
+              result.append({
+                "review_title": review_title.text,
+                "review_content": strip_read_more(review_content.text),
+                "customer_name": customer_name.text,
+                "ratings": ratings.text,
+                "date": date.text,
+              })
+
+    return result
+    #output_file = open("output.json", "a")
+    #output = json.dumps(result, indent=4, sort_keys=True)
+    #output_file.write(output)
